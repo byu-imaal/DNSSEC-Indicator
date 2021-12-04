f57aa3e75aa948f78b2001b365ff932a968b13cc
Trevin Avery <trevinavery@gmail.com>
2021-12-04 13:18:59 -0700

    added images and image resizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WebpackImagesResizer = require('webpack-images-resizer')
+
+const files = ['default', 'loading', 'secure', 'insecure', 'bogus']
+const sizes = [16, 32, 48, 128]
+
+let resizers = []
+sizes.forEach(size => {
+
+	let images = []
+	files.forEach(file => {
+		images.push({
+			src: path.resolve(__dirname, `images/${file}.png`), 
+			dest: `images/${file}/${size}.png`,
+		})
+	})
+	resizers.push(new WebpackImagesResizer(images, {width: size, height: size}))
+})
 
 module.exports = {
 	mode: "production",
@@ -11,7 +28,8 @@ module.exports = {
 			patterns: [
 				{ from: 'static' }
 			]
-		})
+		}),
+        ...resizers,
 	],
 	optimization: {
         minimize: false
